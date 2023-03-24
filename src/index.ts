5351e0696bf4de19af85ce4f951b0a6d5f243d6f
Jake LaCombe <jake.lacombe2@gmail.com>
2023-03-24 14:41:20 -0500

    Initial Project

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,89 @@
-import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera"
-import { Engine } from "@babylonjs/core/Engines/engine"
-import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight"
-import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder"
-import { Scene } from "@babylonjs/core/scene"
-import { Vector3 } from "@babylonjs/core/Maths/math.vector"
+import "@babylonjs/core/Debug/debugLayer";
 
-import { SampleMaterial } from "./Materials/SampleMaterial"
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
+import { Scene } from "@babylonjs/core/scene";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 
-const view = document.getElementById("view") as HTMLCanvasElement
-const engine = new Engine(view, true)
+import { SampleMaterial } from "./Materials/SampleMaterial";
+import { Mesh, SceneLoader, UniversalCamera } from "@babylonjs/core";
+import GalleryScene from "./Blender/building_04_nopack.babylon";
 
-const scene = new Scene(engine)
+class App {
+  canvas: HTMLCanvasElement = document.createElement("canvas");
 
-const camera = new ArcRotateCamera(
-    "camera",
-    Math.PI / 2,
-    Math.PI / 3.2,
-    2,
-    Vector3.Zero(),
-    scene)
+  constructor() {
+    // create the canvas html element and attach it to the webpage
+    this.canvas.style.width = "100%";
+    this.canvas.style.height = "100%";
+    this.canvas.id = "gameCanvas";
+    document.body.appendChild(this.canvas);
+    // initialize babylon scene and engine
+  }
 
-camera.attachControl(view)
+  async init() {
+    const view = document.getElementById("gameCanvas") as HTMLCanvasElement;
+    const engine = new Engine(view, true);
 
-const light = new HemisphericLight(
-    "light",
-    new Vector3(0, 1, 0),
-    scene)
+    let scene = await SceneLoader.LoadAsync(GalleryScene);
 
-const mesh = MeshBuilder.CreateGround("mesh", {}, scene)
+    let camera: UniversalCamera = new UniversalCamera(
+      "UniversalCamera",
+      new Vector3(0, 15, -10),
+      scene
+    );
+    camera.target = new Vector3(-15, 0, 21);
+    camera.attachControl(true);
+    camera.applyGravity = true;
+    camera.ellipsoid = new Vector3(1, 1, 1);
 
-const material =  new SampleMaterial("material", scene)
-mesh.material = material
+    const assumedFramesPerSecond = 60;
+    const earthGravity = -9.81;
+    scene.gravity = new Vector3(0, earthGravity / assumedFramesPerSecond, 0);
+    scene.collisionsEnabled = true;
+    scene.createDefaultXRExperienceAsync({
+      floorMeshes: [
+        scene.getNodeByID("ground_grass") as Mesh,
+        scene.getNodeByID("Stairs 1") as Mesh,
+        scene.getNodeByID("Stairs 2") as Mesh,
+        scene.getNodeByID("Stairs 3") as Mesh,
+        scene.getNodeByID("Floor 1") as Mesh,
+        scene.getNodeByID("Floor 2") as Mesh,
+        scene.getNodeByID("Floor 3") as Mesh,
+      ],
+    });
 
-engine.runRenderLoop(() => {
-    scene.render();
-})
+    camera.checkCollisions = true;
+
+    // new HemisphericLight("light1", new Vector3(20, 200, 20), scene);
+    // new PointLight("Spot0", new Vector3(0, 100, 5), scene);
+    // var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+    // hide/show the Inspector
+    window.addEventListener("keydown", (ev) => {
+      // Shift+Ctrl+Alt+I
+      if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+        if (scene.debugLayer.isVisible()) {
+          scene.debugLayer.hide();
+        } else {
+          scene.debugLayer.show();
+        }
+      }
+    });
+
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+    const mesh = MeshBuilder.CreateGround("mesh", {}, scene);
+
+    const material = new SampleMaterial("material", scene);
+    mesh.material = material;
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  }
+}
+
+let app = new App();
+app.init();
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,12 +1,12 @@
-const path = require("path")
-const CopyPlugin = require("copy-webpack-plugin")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".glsl"],
@@ -15,16 +15,25 @@ module.exports = {
     rules: [
       { test: /\.tsx?$/, loader: "ts-loader" },
       { test: /\.glsl$/, loader: "webpack-glsl-loader" },
+      {
+        test: /\.(png|jpe?g|gif|glb|gltf|babylon)$/i,
+        loader: "file-loader",
+        options: {
+          publicPath: "./",
+          name: "[name].[ext]",
+        },
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
-        patterns: [
-            { from: "public" },
-        ],
+      patterns: [
+        { from: "src/blender/materials_house", to: "materials_house" },
+        { from: "public" },
+      ],
     }),
     new HtmlWebpackPlugin({
       template: "!!handlebars-loader!src/index.hbs",
     }),
   ],
-}
+};
